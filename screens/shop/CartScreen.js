206aa66ef22621692892ca20e7f083fdc89c6d59
Vladimir Pavković <vladimirpavk@gmail.com>
2020-07-03 11:45:59 +0200

    +

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -3,15 +3,39 @@ import React from 'react';
 import {
     View,
     Text,
-    StyleSheet
-} from 'react-native';
+    StyleSheet,
+    FlatList
+} from 'react-native'
+
+import { connect } from 'react-redux';
+
+import { Ionicons } from '@expo/vector-icons';
+
+const CartItem = (props)=>{
+    const itemPrice=props.item.qty*props.item.price;
+    return(
+        <View style={styles.cartItem}>
+            <Text style={styles.cartItemText}>{props.item.title} ${props.item.price} x {props.item.qty} -<Text style={styles.cartItemText1}> ${itemPrice}</Text></Text>
+            <Ionicons name='md-trash' size={25} color='red'/>
+        </View>
+    );
+}
 
 const CartScreen = (props)=>{
     return(
         <View style={styles.container}>
-            <Text>
-                This is cart screen
-            </Text>
+           <FlatList
+                style={styles.cartListStyle}
+                data={props.cart.items}
+                keyExtractor={(item=>item.id)}
+                renderItem={
+                    (itemData)=>{                       
+                        return(
+                            <CartItem item={itemData.item} />
+                        )
+                    }
+                }
+            />
         </View>
     )
 }
@@ -19,9 +43,41 @@ const CartScreen = (props)=>{
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        justifyContent:'center',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
         alignItems: 'center'
+    },
+    cartItem:{
+       width: '100%',
+       flexDirection: 'row',
+       justifyContent: 'flex-start',
+       alignItems: 'center',
+       marginBottom: 10,
+       padding: 10
+    },
+    cartItemText:{
+        width:'80%',
+        fontFamily: 'roboto',
+        fontSize: 20,
+        borderColor: 'red',
+        borderWidth: 2
+    },
+    cartItemText1:{
+        fontFamily: 'roboto-bold',
+        fontWeight: 'bold'        
+    },
+    cartListStyle:{
+        flex:1,
+        width: '100%',
+        borderColor: 'red',
+        borderWidth: 2
     }
 })
 
-export default CartScreen;
+const mapStateToProps = (state)=>{    
+    return{
+        'cart': state.cart
+    }
+}
+
+export default connect(mapStateToProps)(CartScreen);
